49e03d30bb9b5742199fccc15ae9c97d4e74f1f1
daninogales30 <dnogmat3011@fp.iesromerovargas.com>
2024-11-06 12:17:02 +0100

    Ejercicio 7 hecho

---
+++ ej7/ej7.js
@@ -0,0 +1,13 @@
+let arrayVocales = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+let letra = prompt("Introduce una letra")
+
+function esVocal(letra){
+    for(let i = 0; i<arrayVocales.length; i++){
+        if(letra == arrayVocales[i]){
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(esVocal(letra))
